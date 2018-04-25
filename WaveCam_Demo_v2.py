148f99321d0d1950312e78d3c129a18ccf90120f
Sule, Nishant V <nsule@seas.harvard.edu>
2018-04-25 12:05:29 -0400

    fixed Mur radiaion boundaries

--- WaveCam_Demo_v2.py
+++ WaveCam_Demo_v2.py
@@ -13,15 +13,15 @@ wavelmin = c0[0] / freq
 class fdtdVar:
     def __init__(self, rs, cs):
         # Constants
-        cn = 1.0 / np.sqrt(2.0)  # Courant number
+        cn = 0.9 / np.sqrt(2.0)  # Courant number
         # Variables
         self.r = np.int(rs)  # number of rows
         self.c = np.int(cs)  # number of columns
         self.freq = freq  # frequency of source
-        temp = (self.r, self.c + 1)
+        temp = (self.r - 1, self.c)
         self.vx = np.zeros(temp)  # velocity along x
         self.mvx = np.zeros(temp, dtype=np.int8)
-        temp = (self.r + 1, self.c)
+        temp = (self.r, self.c - 1)
         self.vy = np.zeros(temp)  # velocity along y
         self.mvy = np.zeros(temp, dtype=np.int8)
         temp = (self.r, self.c)
@@ -39,11 +39,11 @@ class fdtdVar:
             self.db[i] = self.dt / (rho[i] * self.dx)
         self.da[1] = 0
         temp = (self.r, 2, 2)
-        self.vxl = np.zeros(temp)
-        self.vxr = np.zeros(temp)
+        self.prl = np.zeros(temp)
+        self.prr = np.zeros(temp)
         temp = (self.c, 2, 2)
-        self.vyb = np.zeros(temp)
-        self.vyt = np.zeros(temp)
+        self.prb = np.zeros(temp)
+        self.prt = np.zeros(temp)
         rtemp = np.arange(0, self.r, 1)
         ctemp = np.arange(0, self.c, 1)
         rm, cm = np.meshgrid(rtemp, ctemp)
@@ -62,17 +62,23 @@ class fdtdVar:
                                        - self.cb[self.mpr[1:rm - 1, 1:cm - 1]] * prs
                                        * self.gaussamp[1:rm - 1, 1:cm - 1] / self.dx)
 
-    def fdtd_update(self):
+    def fdtd_update_pr(self):
         ri = self.r
         ci = self.c
-        self.pr[0:ri, 0:ci] = (self.ca[self.mpr[0:ri, 0:ci]] * self.pr[0:ri, 0:ci]
-                               - self.cb[self.mpr[0:ri, 0:ci]]
-                               * ((self.vx[0:ri, 1:ci + 1] - self.vx[0:ri, 0:ci])
-                                  + (self.vy[1:ri + 1, 0:ci] - self.vy[0:ri, 0:ci])))
-        self.vx[0:ri, 1:ci] = (self.da[self.mvx[0:ri, 1:ci]] * self.vx[0:ri, 1:ci]
-                               - self.db[self.mvx[0:ri, 1:ci]] * (self.pr[0:ri, 1:ci] - self.pr[0:ri, 0:ci - 1]))
-        self.vy[1:ri, 0:ci] = (self.da[self.mvy[1:ri, 0:ci]] * self.vy[1:ri, 0:ci]
-                               - self.db[self.mvy[1:ri, 0:ci]] * (self.pr[1:ri, 0:ci] - self.pr[0:ri - 1, 0:ci]))
+        self.pr[1:ri - 1, 1:ci - 1] = (self.ca[self.mpr[1:ri - 1, 1:ci - 1]] * self.pr[1:ri - 1, 1:ci - 1]
+                                       - self.cb[self.mpr[1:ri - 1, 1:ci - 1]]
+                                       * ((self.vx[1:ri - 1, 1:ci - 1] - self.vx[0:ri - 2, 1:ci - 1])
+                                       + (self.vy[1:ri - 1, 1:ci - 1] - self.vy[1:ri - 1, 0:ci - 2])))
+
+    def fdtd_update_v(self):
+        ri = self.r
+        ci = self.c
+        self.vx[0:ri - 1, 0:ci] = (self.da[self.mvx[0:ri - 1, 0:ci]] * self.vx[0:ri - 1, 0:ci]
+                                   - self.db[self.mvx[0:ri - 1, 0:ci]]
+                                   * (self.pr[1:ri, 0:ci] - self.pr[0:ri - 1, 0:ci]))
+        self.vy[0:ri, 0:ci - 1] = (self.da[self.mvy[0:ri, 0:ci - 1]] * self.vy[0:ri, 0:ci - 1]
+                                   - self.db[self.mvy[0:ri, 0:ci - 1]]
+                                   * (self.pr[0:ri, 1:ci] - self.pr[0:ri, 0:ci - 1]))
 
     def boundary(self):
         ri = self.r
@@ -81,52 +87,47 @@ class fdtdVar:
         c2 = 2 * self.dx / (c0[0] * self.dt + self.dx)
         c3 = (c0[0] * self.dt) ** 2 / (2 * self.dx * (c0[0] * self.dt + self.dx))
         # Left and right boundaries
-        self.vx[1:ri - 1, 0] = (-self.vxl[1:ri - 1, 1, 1]
-                                + c1 * (self.vx[1:ri - 1, 1] + self.vxl[1:ri - 1, 0, 1])
-                                + c2 * (self.vxl[1:ri - 1, 0, 0] + self.vxl[1:ri - 1, 1, 0])
-                                + c3 * (self.vxl[2:ri, 0, 0] - 2 * self.vxl[1:ri - 1, 0, 0]
-                                        + self.vxl[0:ri - 2, 0, 0] + self.vxl[2:ri, 1, 0]
-                                        - 2 * self.vxl[1:ri - 1, 1, 0] + self.vxl[0:ri - 2, 1, 0]))
-        self.vx[1:ri - 1, ci] = (-self.vxr[1:ri - 1, 1, 1]
-                                 + c1 * (self.vx[1:ri - 1, ci - 1] + self.vxr[1:ri - 1, 0, 1])
-                                 + c2 * (self.vxr[1:ri - 1, 0, 0] + self.vxr[1:ri - 1, 1, 0])
-                                 + c3 * (self.vxr[2:ri, 0, 0] - 2 * self.vxr[1:ri - 1, 0, 0]
-                                         + self.vxr[0:ri - 2, 0, 0] + self.vxr[2:ri, 1, 0]
-                                         - 2 * self.vxr[1:ri - 1, 1, 0] + self.vxr[0:ri - 2, 1, 0]))
-
-        # Bottom and top boundaries
-        self.vy[0, 1:ci - 1] = (-self.vyb[1:ci - 1, 1, 1]
-                                + c1 * (self.vy[1, 1:ci - 1] + self.vyb[1:ci - 1, 0, 1])
-                                + c2 * (self.vyb[1:ci - 1, 0, 0] + self.vyb[1:ci - 1, 1, 0])
-                                + c3 * (self.vyb[2:ci, 0, 0] - 2 * self.vyb[1:ci - 1, 0, 0]
-                                        + self.vyb[0:ci - 2, 0, 0] + self.vyb[2:ci, 1, 0]
-                                        - 2 * self.vyb[1:ci - 1, 1, 0] + self.vyb[0:ci - 2, 1, 0]))
-        self.vy[ri, 1:ci - 1] = (-self.vyt[1:ci - 1, 1, 1]
-                                 + c1 * (self.vy[ri - 1, 1:ci - 1] + self.vyt[1:ci - 1, 0, 1])
-                                 + c2 * (self.vyt[1:ci - 1, 0, 0] + self.vyt[1:ci - 1, 1, 0])
-                                 + c3 * (self.vyt[2:ci, 0, 0] - 2 * self.vyt[1:ci - 1, 0, 0]
-                                         + self.vyt[0:ci - 2, 0, 0] + self.vyt[2:ci, 1, 0]
-                                         - 2 * self.vyt[1:ci - 1, 1, 0] + self.vyt[0:ci - 2, 1, 0]))
-        Corners
-        self.vx[0, 0] = self.vxl[1, 1, 1]
-        self.vx[ri - 1, 0] = self.vxl[ri - 2, 1, 1]
-        self.vx[0, ci] = self.vxr[1, 1, 1]
-        self.vx[ri - 1, ci] = self.vxr[ri - 2, 1, 1]
-        self.vy[0, 0] = self.vyb[1, 1, 1]
-        self.vy[0, ci - 1] = self.vyb[ci - 2, 1, 1]
-        self.vy[ri, 0] = self.vyt[1, 1, 1]
-        self.vy[ri, ci - 1] = self.vyt[ci - 2, 1, 1]
+        self.pr[1:ri - 1, 0] = (-self.prl[1:ri - 1, 1, 1]
+                                + c1 * (self.pr[1:ri - 1, 1] + self.prl[1:ri - 1, 0, 1])
+                                + c2 * (self.prl[1:ri - 1, 0, 0] + self.prl[1:ri - 1, 1, 0])
+                                + c3 * (self.prl[2:ri, 0, 0] - 2 * self.prl[1:ri - 1, 0, 0] + self.prl[0:ri - 2, 0, 0]
+                                        + self.prl[2:ri, 1, 0] - 2 * self.prl[1:ri - 1, 1, 0]
+                                        + self.prl[0:ri - 2, 1, 0]))
+        self.pr[1:ri - 1, ci - 1] = (-self.prr[1:ri - 1, 1, 1]
+                                     + c1 * (self.pr[1:ri - 1, ci - 2] + self.prr[1:ri - 1, 0, 1])
+                                     + c2 * (self.prr[1:ri - 1, 0, 0] + self.prr[1:ri - 1, 1, 0])
+                                     + c3 * (self.prr[2:ri, 0, 0] - 2 * self.prr[1:ri - 1, 0, 0]
+                                             + self.prr[0:ri - 2, 0, 0] + self.prr[2:ri, 1, 0]
+                                             - 2 * self.prr[1:ri - 1, 1, 0] + self.prr[0:ri - 2, 1, 0]))
+        # Top and bottom boundaries
+        self.pr[0, 1:ci - 1] = (-self.prt[1:ci - 1, 1, 1]
+                                + c1 * (self.pr[1, 1:ci - 1] + self.prt[1:ci - 1, 0, 1])
+                                + c2 * (self.prt[1:ci - 1, 0, 0] + self.prt[1:ci - 1, 1, 0])
+                                + c3 * (self.prt[2:ci, 0, 0] - 2 * self.prt[1:ci - 1, 0, 0] + self.prt[0:ci - 2, 0, 0]
+                                        + self.prt[2:ci, 1, 0] - 2 * self.prt[1:ci - 1, 1, 0]
+                                        + self.prt[0:ci - 2, 1, 0]))
+        self.pr[ri - 1, 1:ci - 1] = (-self.prb[1:ci - 1, 1, 1]
+                                     + c1 * (self.pr[ri - 2, 1:ci - 1] + self.prb[1:ci - 1, 0, 1])
+                                     + c2 * (self.prb[1:ci - 1, 0, 0] + self.prb[1:ci - 1, 1, 0])
+                                     + c3 * (self.prb[2:ci, 0, 0] - 2 * self.prb[1:ci - 1, 0, 0]
+                                             + self.prb[0:ci - 2, 0, 0] + self.prb[2:ci, 1, 0]
+                                             - 2 * self.prb[1:ci - 1, 1, 0] + self.prb[0:ci - 2, 1, 0]))
+        # Corners
+        self.pr[0, 0] = self.prt[1, 1, 1]
+        self.pr[0, ci - 1] = self.prt[ci - 2, 1, 1]
+        self.pr[ri - 1, 0] = self.prb[1, 1, 1]
+        self.pr[ri - 1, ci - 1] = self.prb[ci - 2, 1, 1]
 
         # Store boundary values
         for i in range(0, 2, 1):
-            self.vxl[0:ri, i, 1] = self.vxl[0:ri, i, 0]
-            self.vxl[0:ri, i, 0] = self.vx[0:ri, i]
-            self.vxr[0:ri, i, 1] = self.vxr[0:ri, i, 0]
-            self.vxr[0:ri, i, 0] = self.vx[0:ri, ci - i]
-            self.vyb[0:ci, i, 1] = self.vyb[0:ci, i, 0]
-            self.vyb[0:ci, i, 0] = self.vy[i, 0:ci]
-            self.vyt[0:ci, i, 1] = self.vyt[0:ci, i, 0]
-            self.vyt[0:ci, i, 0] = self.vy[ri - i, 0:ci]
+            self.prl[0:ri, i, 1] = self.prl[0:ri, i, 0]
+            self.prl[0:ri, i, 0] = self.pr[0:ri, i]
+            self.prr[0:ri, i, 1] = self.prr[0:ri, i, 0]
+            self.prr[0:ri, i, 0] = self.pr[0:ri, ci - 1 - i]
+            self.prt[0:ci, i, 1] = self.prt[0:ci, i, 0]
+            self.prt[0:ci, i, 0] = self.pr[i, 0:ci]
+            self.prb[0:ci, i, 1] = self.prb[0:ci, i, 0]
+            self.prb[0:ci, i, 0] = self.pr[ri - 1 - i, 0:ci]
 
     def update_domain(self):
         self.mvx.fill(0)
@@ -157,29 +158,30 @@ while True:
     # Convert to grayscale
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Webcam frame cleanup
+    # Webcam image cleanup from grayscale to BW
     blurimg = cv2.medianBlur(img, 21)
-    threshimg = cv2.adaptiveThreshold(blurimg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 5)
+    threshimg = cv2.adaptiveThreshold(blurimg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 5)
     img = threshimg / 256.0
 
-    # Clean up edges
-    img[0:5, 0:fs.c] = 1
-    img[fs.r - 5:fs.r, 0:fs.c] = 1
-    img[0:fs.r, 0:5] = 1
-    img[0:fs.r, fs.c - 5:fs.c] = 1
+    # Clean up edges of webcam image
+    img[0:5, 0:columns] = 1
+    img[rows - 5:rows, 0:columns] = 1
+    img[0:rows, 0:5] = 1
+    img[0:rows, columns - 5:columns] = 1
 
-    # Create rigid material
-    imgtemp = np.pad(img, ((0, 0), (0, 1)), "constant", constant_values=1.0)
-    idx = imgtemp < 0.4
+    # Create rigid material from black portions of image
+    imgtemp = img[0:rows - 1, 0:columns]
+    idx = imgtemp < 0.5
     fs.mvx[idx] = 1
-    imgtemp = np.pad(img, ((0, 1), (0, 0)), "constant", constant_values=1.0)
-    idx = imgtemp < 0.4
+    imgtemp = img[0:rows, 0:columns - 1]
+    idx = imgtemp < 0.5
     fs.mvy[idx] = 1
 
     # Update image with FDTD solution
-    fs.fdtd_update()
+    fs.fdtd_update_pr()
     fs.source(tc)
     fs.boundary()
+    fs.fdtd_update_v()
     imgdisp = img + fs.pr
 
     tc = tc + 1
@@ -191,4 +193,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-print(tc)
